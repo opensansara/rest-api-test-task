c3f838c6253e429f42ff5553701f0266609daf99
pavel <itmariacchi@gmail.com>
2020-01-03 16:00:38 +0300

    Добавил миграцию для создания таблицы товаров

--- migrations/20200103124536_AddProductsTable.php
+++ migrations/20200103124536_AddProductsTable.php
@@ -1,21 +1,37 @@
 <?php
-use Phpmig\Migration\Migration;
+use Doctrine\DBAL\DBALException;
 
 class AddProductsTable extends \App\Infrastructure\DB\Migration
 {
     /**
      * Do the migration
+     * @throws DBALException
      */
     public function up()
     {
+        $tableName = $this->getTableName();
 
+        $sql = <<<SQL
+CREATE TABLE `{$tableName}` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `price` INT NULL,
+  PRIMARY KEY (`id`));
+SQL;
+        $this->getConnection()->exec($sql);
     }
 
     /**
      * Undo the migration
+     * @throws DBALException
      */
     public function down()
     {
+        $this->getConnection()->exec('DROP TABLE ' . $this->getTableName());
+    }
 
+    private function getTableName() : string
+    {
+        return 'products';
     }
 }
